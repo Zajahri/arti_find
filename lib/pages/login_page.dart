d48d37e3562baf06b43999034510cc3515fe2484
zajahri <zajahri>
2023-04-13 20:03:55 +0800

    latest

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:arti_find/main.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -14,33 +13,33 @@ class _LoginPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 100.0, horizontal: 100),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                const CircleAvatar(
-                  backgroundImage: AssetImage('img/logo.png'),
-                  radius: 100,
-                ),
-                const SizedBox(
-                  height: 50.0,
-                ),
-                const Text('ArtiFind'),
-                const SizedBox(
-                  height: 250.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const SignIn()),
-                    );
-                  },
-                  child: const Text('Get Started'),
-                ),
-              ]),
-        ));
+      padding: const EdgeInsets.symmetric(vertical: 100.0, horizontal: 100),
+      child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const CircleAvatar(
+              backgroundImage: AssetImage('img/logo.png'),
+              radius: 100,
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
+            const Text('ArtiFind'),
+            const SizedBox(
+              height: 250.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignIn()),
+                );
+              },
+              child: const Text('Get Started'),
+            ),
+          ]),
+    ));
   }
 }
 
@@ -56,40 +55,117 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children:  <Widget>[
+      children: <Widget>[
+        const SizedBox(height: 100),
+        const Text('Sign In'),
+        const SizedBox(height: 50),
+        const TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'User Name',
+          ),
+        ),
+        const SizedBox(height: 50),
+        const TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          child: const Text('Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FirstScreen()),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          child: const Text("Don't have an account?"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignUp()),
+            );
+          },
+        ),
+      ],
+    ));
+  }
+}
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
             const SizedBox(height: 100),
-
-            const Text('Sign In'),
-
+            const Text('Sign Up'),
             const SizedBox(height: 50),
-
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'First Name',
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Middle Name',
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Last Name',
+              ),
+            ),
+            const SizedBox(height: 10),
             const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'User Name',
               ),
             ),
-
-            const SizedBox(height: 50),
-
+            const SizedBox(height: 10),
             const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: 'User Name',
+                labelText: 'Password',
               ),
             ),
-
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Confirm Password',
+              ),
+            ),
+            const SizedBox(height: 100),
             ElevatedButton(
-              child: const Text('Login'),
+              child: const Text('Sign Up'),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                  MaterialPageRoute(builder: (context) => const SignIn()),
                 );
               },
-            )
+            ),
           ],
-        ));
+    ),
+    );
   }
 }
