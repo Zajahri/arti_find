57110880dd42c072a9b86188bec81727fcee1f17
polin <capaciapauline@gmail.com>
2023-05-15 13:51:43 +0800

    upd

--- lib/pages/feedback_form.dart
+++ lib/pages/feedback_form.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-class FeedBack extends StatelessWidget {
+class FeedBack extends StatefulWidget {
   const FeedBack({Key? key}) : super(key: key);
+
+  @override
+  _FeedBackState createState() => _FeedBackState();
+}
+
+class _FeedBackState extends State<FeedBack> {
+  bool _isChecked1 = false;
+  bool _isChecked2 = false;
+  bool _isChecked3 = false;
+  bool _isChecked4 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +21,92 @@ class FeedBack extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: const Text('Feedback Form',
-          style: TextStyle(color: Color(0xFF6F1D1B),fontFamily: 'Montserrat', fontWeight:FontWeight.w700),
+        title: const Text(
+          'Feedback Form',
+          style: TextStyle(
+            color: Color(0xFF6F1D1B),
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w700,
+          ),
         ),
         leading: IconButton(
           onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(Icons.arrow_back_ios, color: Color(0xFF6F1D1B),),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Color(0xFF6F1D1B),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 2.0),
+        child: Table(
+          children: [
+            TableRow(
+              children: [
+                const TableCell(
+                  child: Text(
+                    'Question 1',
+                    style: TextStyle(
+                      color: Color(0xFF6F1D1B),
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+                TableCell(
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: Checkbox(
+                          value: _isChecked1,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked1 = value ?? false;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Checkbox(
+                          value: _isChecked2,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked2 = value ?? false;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Checkbox(
+                          value: _isChecked3,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked3 = value ?? false;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Checkbox(
+                          value: _isChecked4,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked4 = value ?? false;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+          ],
         ),
       ),
     );
   }
 }
-
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -23,7 +23,7 @@ class _SignupPageState extends State<SignupPage> {
         physics: const AlwaysScrollableScrollPhysics(),
         padding: const EdgeInsets.symmetric(
           horizontal: 25,
-          vertical: 120,
+          vertical: 65,
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -47,8 +47,116 @@ class _SignupPageState extends State<SignupPage> {
                 textStyle: const TextStyle(color: Color(0xFF6F1D1B), fontSize: 20,fontWeight: FontWeight.w500),
               ),
             ),
-
-            const SizedBox(height: 20.0),
+            const SizedBox(height: 35.0),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'First Name',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                                offset: Offset(0,2)
+                            )
+                          ],
+                        ),
+                        height: 60,
+                        child: const TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontFamily: 'Montserrat',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.only(top: 14),
+                            prefixIcon: Icon(
+                                Icons.person,
+                                color: Color(0xFFeb8c00)
+                            ),
+                            hintText: 'First Name',
+                            hintStyle: TextStyle(color: Colors.black38),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Last Name',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                                offset: Offset(0,2)
+                            )
+                          ],
+                        ),
+                        height: 60,
+                        child: const TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontFamily: 'Montserrat',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.only(top: 14),
+                            prefixIcon: Icon(
+                                Icons.person,
+                                color: Color(0xFFeb8c00)
+                            ),
+                            hintText: 'Last Name',
+                            hintStyle: TextStyle(color: Colors.black38),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0),
             Container(
               alignment: Alignment.centerLeft,
               child: const Text(
@@ -98,61 +206,7 @@ class _SignupPageState extends State<SignupPage> {
                 ),
               ),
             ),
-
             const SizedBox(height:10),
-
-            Container(
-              alignment: Alignment.centerLeft,
-              child: const Text(
-                'Username',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Montserrat',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            Container(
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 6,
-                      offset: Offset(0,2)
-                  )
-                ],
-              ),
-              height: 60,
-              child: const TextField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
-                    color: Colors.black54,
-                    fontFamily: 'Montserrat',
-                    fontSize: 18,
-                    fontWeight:FontWeight.w500),
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding:EdgeInsets.only(top: 14),
-                    prefixIcon: Icon(
-                        Icons.person,
-                        color:Color(0xFFeb8c00)
-                    ),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                        color: Colors.black38
-                    )
-                ),
-              ),
-            ),
-
-            const SizedBox(height:10),
-
             Container(
               alignment: Alignment.centerLeft,
               child: const Text(
@@ -280,7 +334,7 @@ class _SignupPageState extends State<SignupPage> {
               ),
             ),
 
-            const SizedBox(height:50),
+            const SizedBox(height:30),
             Container(
               height: 50,
               decoration: BoxDecoration(
