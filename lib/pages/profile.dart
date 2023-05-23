50872c37d46d6c57c62cd90e5f5a99a62e48dcda
polin <capaciapauline@gmail.com>
2023-05-23 16:37:31 +0800

    u

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -9,6 +9,111 @@ class Profile extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFE9DBC7),
       appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: ListView(
+          padding: const EdgeInsets.all(15),
+          children: [
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 4, color: Colors.blue),
+                      boxShadow: [
+                        BoxShadow(
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                          color: Colors.black.withOpacity(0.1),
+                        )
+                      ],
+                      shape: BoxShape.circle,
+                      image: const DecorationImage(
+                        image: AssetImage('img/tao.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                'Moises Fernandez',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Center(
+              child: Text(
+                'example@email.com',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ProfileEdit(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF6F1D1B),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 4.0,
+                ),
+                child: const Text(
+                  'Edit Profile',
+                  style: TextStyle(
+                    color: Color(0xFFE9DBC7),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class ProfileEdit extends StatefulWidget {
+  const ProfileEdit({super.key});
+
+  @override
+  _ProfileEditState createState() => _ProfileEditState();
+}
+
+class _ProfileEditState extends State<ProfileEdit> {
+  bool isObscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE9DBC7),
+      appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
@@ -28,13 +133,126 @@ class Profile extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Profile(
-          name: "Shamim Miah",
-          website: "shamimmiah.com",
-          designation: "Project Manager | Flutter & Blockchain Developer",
-          email: "cse.shamimosmanpailot@gmail.com",
-          phone_number: "01757736053",
+      body: Container(
+        padding: const EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.blue),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.1),
+                          )
+                        ],
+                        shape: BoxShape.circle,
+                        image: const DecorationImage(
+                          image: AssetImage('img/tao.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 4, color: Colors.white),
+                          color: Colors.blue,
+                        ),
+                        child: const Icon(
+                          Icons.camera_alt_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              buildTextField("First Name", "Moises", false),
+              buildTextField("Last Name", "Fernandez", false),
+              buildTextField("Email", "example@email.com", false),
+              buildTextField("Phone", "0909837412314", false),
+              buildTextField("Gender", "Male", false),
+              buildTextField("Password", "*******", true),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(horizontal: 50),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                      ),
+                      child:const Text("Cancel", style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.black
+                      ))
+                  ),
+                  ElevatedButton(onPressed: () {},
+                    style:  ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                    ),
+                    child: const Text("SAVE", style:TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.black
+                    )),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: TextField(
+        obscureText: isPasswordTextField ? isObscurePassword : false,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField ?
+            IconButton(
+              icon: Icon(
+                isObscurePassword ? Icons.visibility_off : Icons.visibility,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  isObscurePassword = !isObscurePassword;
+                });
+              },
+            )
+                : null,
+            contentPadding: const EdgeInsets.only(bottom:5),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey
+            )
         ),
       ),
     );
